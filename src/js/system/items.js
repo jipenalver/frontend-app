d1510405b38d27cc56a4fc5c2a1063256c0aa614
ianpenalver <jabezpenalver2k20@gmail.com>
2023-12-16 17:51:01 +0800

    Added delete and show by id functionality

--- src/js/system/items.js
+++ src/js/system/items.js
@@ -13,11 +13,16 @@ const btn_logout = document.getElementById("btn_logout");
 
 btn_logout.onclick = doLogout;
 
-// Submit Form Functionality
+// Submit Form Functionality; Both Functional for Create and Update
 const form_item = document.getElementById("form_item");
 
 form_item.onsubmit = async (e) => {
   e.preventDefault();
+
+  // Disable Button
+  document.querySelector("#form_item button[type='submit']").disabled = true;
+  document.querySelector("#form_item button[type='submit']").innerHTML = `
+                      <span>Loading...</span>`;
 
   // Get All values from input, select, textarea under form tag
   const formData = new FormData(form_item);
@@ -50,6 +55,12 @@ form_item.onsubmit = async (e) => {
 
   // Reset Form
   form_item.reset();
+
+  // Enable Submit Button
+  document.querySelector("#form_item button[type='submit']").disabled = false;
+  document.querySelector(
+    "#form_item button[type='submit']"
+  ).innerHTML = `Submit`;
 };
 
 // Load Data Functionality
@@ -100,4 +111,75 @@ async function getDatas() {
 
   // Assign container to the html element to be displayed
   document.getElementById("get_data").innerHTML = container;
+
+  // Assign click event on Edit Btns
+  document.querySelectorAll("#btn_edit").forEach((element) => {
+    element.addEventListener("click", editAction);
+  });
+
+  // Assign click event on Delete Btns
+  document.querySelectorAll("#btn_delete").forEach((element) => {
+    element.addEventListener("click", deleteAction);
+  });
 }
+
+// Delete Functionality
+const deleteAction = async (e) => {
+  const id = e.target.getAttribute("data-id");
+
+  // Change background color the card that you want to delete
+  document.querySelector(`.card[data-id="${id}"]`).style.backgroundColor =
+    "red";
+
+  // Supabase Delete row
+  const { error } = await supabase.from("items").delete().eq("id", id);
+
+  if (error == null) {
+    successNotification("Item Successfully Deleted!", 15);
+
+    // Reload Datas
+    //getDatas(); // This is slow
+
+    // Remove the Card from the list
+    document.querySelector(`.card[data-id="${id}"]`).remove();
+  } else {
+    errorNotification("Something wrong happened. Cannot delete item.", 15);
+    console.log(error);
+
+    // Change background color the card that you want to delete
+    document.querySelector(`.card[data-id="${id}"]`).style.backgroundColor =
+      "white";
+  }
+};
+
+// Edit Functionality; but show first
+const editAction = async (e) => {
+  const id = e.target.getAttribute("data-id");
+
+  // Change background color the card that you want to delete
+  document.querySelector(`.card[data-id="${id}"]`).style.backgroundColor =
+    "yellow";
+
+  // Supabase show by id
+  let { data: items, error } = await supabase
+    .from("items")
+    .select("*")
+    .eq("id", id);
+
+  if (error == null) {
+    // Assign values to the form
+    document.getElementById("item_name").value = items[0].item_name;
+    document.getElementById("price").value = items[0].price;
+    document.getElementById("description").value = items[0].description;
+  } else {
+    errorNotification("Something wrong happened. Cannot show item.", 15);
+    console.log(error);
+
+    // Change background color the card that you want to delete
+    document.querySelector(`.card[data-id="${id}"]`).style.backgroundColor =
+      "white";
+  }
+
+  // Show Modal Form
+  document.getElementById("modal_show").click();
+};
